27354b17ba3e1bd37722aff736d8b91ca01c2db7
Vitaliy-Danilchik <danilchikvvv@gmail.com>
2022-11-07 00:13:51 +0200

    download

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import { Provider } from 'react-redux';
+import { store, persistor } from 'redux/store';
+import App from 'components/App';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import './styles/index.css';
+import Loader from 'components/Loader';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <PersistGate loading={<Loader />} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
 );
+
+// basename = '/goit-react-hw-08-phonebook/';
